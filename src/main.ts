633fe5c02c6bb0444a4aaf68bc3931007537313c
Unai <umendizabal@gmail.com>
2024-03-15 14:25:58 +0100

    terminado

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,36 @@
-import "./style.css";
+const numeroTurno = document.getElementById("numero-turno");
+const btnAnterior = document.getElementById("btn-anterior");
+const btnReset = document.getElementById("btn-reset");
+const btnSiguiente = document.getElementById("btn-siguiente");
+const inputTurno = document.getElementById("input-turno") as HTMLInputElement;
+const btnInsertTurno = document.getElementById("insert-turno");
 
-console.log("Hello Typescript!");
+let turno: number = 1;
+
+const anteriorTurno = () => {
+	turno > 1 && turno--;
+	insertTurno();
+};
+
+const resetTurno = () => {
+	turno = 1;
+	insertTurno();
+};
+
+const siguienteTurno = () => {
+	turno < 99 ? turno++ : (turno = 1);
+	insertTurno();
+};
+
+const insertTurno = () => {
+	if (numeroTurno) numeroTurno.innerHTML = turno.toString().padStart(2, "0");
+};
+
+btnAnterior?.addEventListener("click", anteriorTurno);
+btnReset?.addEventListener("click", resetTurno);
+btnSiguiente?.addEventListener("click", siguienteTurno);
+btnInsertTurno?.addEventListener("click", () => {
+	turno = parseInt(inputTurno.value);
+	inputTurno.value = "";
+	insertTurno();
+});
